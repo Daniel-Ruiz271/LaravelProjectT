ed6598f38af23968db9eeda31bf896a85c7bf88d
DanielRuiz <A313967@alumnos.uaslp.mx>
2025-05-27 13:24:54 -0600

    correcciones +

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       
         <div class="min-h-screen bg-gray-100">
             {{-- @include('layouts.navigation') --}}
 
